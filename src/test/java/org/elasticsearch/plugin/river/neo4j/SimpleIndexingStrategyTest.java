69873133c7cb4c7adf86ba7df57338524d010a32
sksamuel <sam@sksamuel.com>
2013-12-03 23:56:38 +0000

    Added test

--- src/test/java/org/elasticsearch/plugin/river/neo4j/SimpleIndexingStrategyTest.java
+++ src/test/java/org/elasticsearch/plugin/river/neo4j/SimpleIndexingStrategyTest.java
@@ -2,7 +2,10 @@ package org.elasticsearch.plugin.river.neo4j;
 
 import org.elasticsearch.action.index.IndexRequest;
 import org.junit.Test;
+import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.ResourceIterable;
+import org.neo4j.graphdb.ResourceIterator;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -29,12 +32,44 @@ public class SimpleIndexingStrategyTest {
         when(node.getProperty("name")).thenReturn("chris martin");
         when(node.getProperty("location")).thenReturn("hampstead");
         when(node.getProperty("band")).thenReturn("coldplay");
+        when(node.getLabels()).thenReturn(new ResourceIterable<Label>() {
+            @Override
+            public ResourceIterator<Label> iterator() {
+                return new ResourceIterator<Label>() {
+
+                    private int n = 0;
+
+                    @Override
+                    public void close() {
+
+                    }
+                    @Override
+                    public boolean hasNext() {
+                        return n == 0;
+                    }
+                    @Override
+                    public Label next() {
+                        return new Label() {
+                            @Override
+                            public String name() {
+                                return "sammy";
+                            }
+                        };
+                    }
+                    @Override
+                    public void remove() {
+
+                    }
+                };
+            }
+        });
 
         IndexRequest req = s.build("neo4j-index", "node", node, 12);
         assertEquals("neo4j-index", req.index());
         assertEquals("node", req.type());
         assertEquals(12, req.sourceAsMap().get("version"));
         assertEquals(String.valueOf(id), req.id());
+        assertEquals("sammy", req.sourceAsMap().get("label"));
         assertEquals("coldplay", req.sourceAsMap().get("band"));
         assertEquals("hampstead", req.sourceAsMap().get("location"));
         assertEquals("chris martin", req.sourceAsMap().get("name"));
